addb26085fdbc24599a36e0dc1cab06bb1d1ab14
prudhvi <prudhvieethakota123@gmail.com>
2021-05-14 11:10:48 +0530

    Added: Code for JavaCollectionDemo-StackList

--- main/java/com/javacollectiondemo/JavaCollectionDemo.java
+++ main/java/com/javacollectiondemo/JavaCollectionDemo.java
@@ -4,10 +4,12 @@ import javax.swing.text.html.HTMLDocument;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class JavaCollectionDemo {
     public static void main(String[] args) {
         doListDemo();
+        doStackDemo();
     }
     private static void doListDemo() {
         System.out.println("In doListDemo");
@@ -22,5 +24,26 @@ public class JavaCollectionDemo {
         list.remove(2);
         list.add("Krishna");
         System.out.println(list);
+    }
+    private static void doStackDemo() {
+        System.out.println("\nIndoStackDemo");
+        Stack<String> stack = new Stack<>();
+        stack.push("Ayesha");
+        stack.push("Ganesh");
+        stack.push("Amit");
+        stack.push("Garima");
+        stack.push("Satya");
+        System.out.println(stack);
+        String pop = stack.pop();
+        System.out.println(pop);
+        System.out.println(stack);
+        String peek = stack.peek();
+        System.out.println(peek);
+        int search = stack.search("Krishna");
+        if(search == -1) {
+            System.out.println("NotFound");
+        } else {
+            System.out.println("Found");
+        }
     }
 }
